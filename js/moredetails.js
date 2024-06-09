c55c567a7733fe5e3526dffc648b3aec68b94dda
Rafia-dam <134529120+Rafia-dam@users.noreply.github.com>
2024-06-09 17:18:25 +0700

    menambahkan konektifitas

--- js/moredetails.js
+++ js/moredetails.js
@@ -2,13 +2,44 @@ document.getElementById('actionSelect').addEventListener('change', function() {
   var selectedValue = this.value;
   var divs = document.getElementsByClassName('toggleDiv');
 
-  // Hide all divs
   for (var i = 0; i < divs.length; i++) {
     divs[i].style.display = 'none';
 }
 
-// Show the selected div if it's not 'none'
 if (selectedValue !== 'none') {
     document.getElementById(selectedValue).style.display = 'grid';
 }
 });
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+if (getUrlParameter('showdiv1') === 'true') {
+  document.getElementById('div1').style.display = 'grid';
+}
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+if (getUrlParameter('showdiv2') === 'true') {
+  document.getElementById('div2').style.display = 'grid';
+}
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+if (getUrlParameter('showdiv3') === 'true') {
+  document.getElementById('div3').style.display = 'grid';
+}
