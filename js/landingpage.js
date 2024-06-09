8a9829a72416cb67e3801be85bb457879fc06d80
Rafia-dam <134529120+Rafia-dam@users.noreply.github.com>
2024-06-09 17:00:12 +0700

    merubah form dan memperbaiki foto di more details

--- js/landingpage.js
+++ js/landingpage.js
@@ -17,6 +17,38 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
-document.getElementById("submit").addEventListener("click", function() {
-  alert("Thank You For Your Feedback");
-});
+document.getElementById('contact').onsubmit = function(event) {
+  event.preventDefault(); 
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = document.getElementById('message').value;
+
+
+  document.getElementById('formData').innerText = `Do you want to proceed with the following information?
+  \n\nName: ${name}\nEmail: ${email}\nPhone Number: ${phone}\nmessage: ${message}`;
+
+  document.getElementById('customModal').style.display = 'block';
+};
+
+document.getElementById('closeModal').onclick = function() {
+  document.getElementById('customModal').style.display = 'none';
+};
+
+document.getElementById('confirmButton').onclick = function() {
+  alert('Form submitted successfully!');
+  document.getElementById('customModal').style.display = 'none';
+  document.getElementById('contact').submit();
+};
+
+document.getElementById('cancelButton').onclick = function() {
+  alert('Form submission cancelled.');
+  document.getElementById('customModal').style.display = 'none';
+};
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById('customModal')) {
+      document.getElementById('customModal').style.display = 'none';
+  }
+};
